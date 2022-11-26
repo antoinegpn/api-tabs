d277ceb0da1402a326632afaaf1aeae29d2f6c10
Antoine Grappin <antoine.grappin@epitech.eu>
2022-11-27 00:48:58 +0100

    Add new routes

--- app.js
+++ app.js
@@ -19,11 +19,38 @@ app.use('/api/song_by_id/:id',(req, res) => {
    })
 });
 
+//SEARCH song by title
+app.use('/api/song_by_title/:title',(req, res) => {
+    let title = req.params.title;
+    db.all(`SELECT ID, TITLE FROM songs WHERE TITLE LIKE '%${title}%'`, function (err, rows) {
+       res.json(rows)
+   })
+});
+
 //POST new song
 app.use('/api/add_song/:title',(req, res) => {
     let title = req.params.title;
     db.run(`INSERT INTO songs (TITLE) VALUES (?)`, [title], function (err) {
         console.log(title)
+       res.json({message: 'success'})
+   })
+});
+
+//UPDATE one song 
+app.use('/api/update_song/:id/:title',(req, res) => {
+    let id = req.params.id;
+    let title = req.params.title;
+    db.run(`UPDATE songs SET title = ? WHERE id = ?`, [title, id], function (err) {
+        console.log(title)
+       res.json({message: 'success'})
+   })
+});
+
+//DELETE a song (id)
+app.use('/api/delete_song/:id',(req, res) => {
+    let id = req.params.id;
+    db.run(`DELETE FROM songs WHERE ID = ${id}`, function (err) {
+        console.log(id)
        res.json({message: 'success'})
    })
 });
@@ -44,11 +71,38 @@ app.use('/api/artist_by_id/:id',(req, res) => {
    })
 });
 
+//SEARCH artist by title
+app.use('/api/artist_by_title/:title',(req, res) => {
+    let title = req.params.title;
+    db.all(`SELECT ID, TITLE FROM artists WHERE TITLE LIKE '%${title}%'`, function (err, rows) {
+       res.json(rows)
+   })
+});
+
 //POST new artist
 app.use('/api/add_artist/:title',(req, res) => {
     let title = req.params.title;
     db.run(`INSERT INTO artists (TITLE) VALUES (?)`, [title], function (err) {
         console.log(title)
+       res.json({message: 'success'})
+   })
+});
+
+//UPDATE one artist 
+app.use('/api/update_artist/:id/:title',(req, res) => {
+    let id = req.params.id;
+    let title = req.params.title;
+    db.run(`UPDATE artists SET title = ? WHERE id = ?`, [title, id], function (err) {
+        console.log(title)
+       res.json({message: 'success'})
+   })
+});
+
+//DELETE an artist (id)
+app.use('/api/delete_artist/:id',(req, res) => {
+    let id = req.params.id;
+    db.run(`DELETE FROM artists WHERE ID = ${id}`, function (err) {
+        console.log(id)
        res.json({message: 'success'})
    })
 });
@@ -69,6 +123,14 @@ app.use('/api/tuning_by_id/:id',(req, res) => {
    })
 });
 
+//SEARCH tuning by libelle
+app.use('/api/tuning_by_libelle/:libelle',(req, res) => {
+    let libelle = req.params.libelle;
+    db.all(`SELECT ID, LIBELLE FROM tunings WHERE LIBELLE LIKE '%${libelle}%'`, function (err, rows) {
+       res.json(rows)
+   })
+});
+
 //POST new tuning
 app.use('/api/add_tuning/:libelle',(req, res) => {
     let libelle = req.params.libelle;
@@ -78,10 +140,22 @@ app.use('/api/add_tuning/:libelle',(req, res) => {
    })
 });
 
-//Generate tables
-app.use('/api/create_tables',(req, res) => {
-    db.run("CREATE TABLE 'artists' ('id' INTEGER, 'title' TEXT, PRIMARY KEY('id' AUTOINCREMENT));CREATE TABLE 'songs' ('id' INTEGER, 'title' TEXT, PRIMARY KEY('id' AUTOINCREMENT));CREATE TABLE 'tunings' ('id' INTEGER, 'libelle' TEXT, PRIMARY KEY('id' AUTOINCREMENT))", function (err, rows) {
-        console.log('success');
+//UPDATE one tuning 
+app.use('/api/update_tuning/:id/:libelle',(req, res) => {
+    let id = req.params.id;
+    let libelle = req.params.libelle;
+    db.run(`UPDATE tunings SET libelle = ? WHERE id = ?`, [libelle, id], function (err) {
+        console.log(libelle)
+       res.json({message: 'success'})
+   })
+});
+
+//DELETE a tuning (id)
+app.use('/api/delete_tuning/:id',(req, res) => {
+    let id = req.params.id;
+    db.run(`DELETE FROM tunings WHERE ID = ${id}`, function (err) {
+        console.log(id)
+       res.json({message: 'success'})
    })
 });
 
